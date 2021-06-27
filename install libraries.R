987163ffbdbe05c42136cdb6f1c923e3f3512a20
Computerfreaked <computerfreaked@hotmail.com>
2021-06-27 17:21:53 +0200

    added library to increase chance of correctly installing ggplot2

--- install libraries.R
+++ install libraries.R
@@ -3,7 +3,7 @@
 # See https://stackoverflow.com/questions/4090169/elegant-way-to-check-for-missing-packages-and-install-them
 #
 
-list.of.packages <- c("readxl", "stringr", "dplyr", "purrr", "ggplot2", "rmarkdown", "parallel", "tinytex")
+list.of.packages <- c("readxl", "stringr", "dplyr", "purrr", "colorspace", "ggplot2", "rmarkdown", "parallel", "tinytex")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages)
 
